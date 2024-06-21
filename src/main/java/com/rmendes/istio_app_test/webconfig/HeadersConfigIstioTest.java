bd0e541309887f0ce61c3130600e36dfc184e29c
Raffael Mendes <rmendes@redhat.com>
2024-06-21 14:10:35 -0300

    deleting some configs

--- src/main/java/com/rmendes/istio_app_test/webconfig/HeadersConfigIstioTest.java
+++ src/main/java/com/rmendes/istio_app_test/webconfig/HeadersConfigIstioTest.java
@@ -1,6 +1,5 @@
 package com.rmendes.istio_app_test.webconfig;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -19,20 +18,4 @@ public class HeadersConfigIstioTest implements WebMvcConfigurer{
 		.allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
 		.maxAge(3600);
 	}
-	
-	   @Bean
-	    public WebMvcConfigurer corsConfig() {
-	        return new WebMvcConfigurer() {
-	            @Override
-	            public void addCorsMappings(CorsRegistry registry) {
-	                registry
-	                        .addMapping("/**")
-	                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
-	                        .allowedHeaders("Origin", "Access-Control-Allow-Origin", "Content-Type", "Accept", "Authorization", "X-Requested-With","x-b3-traceid", "x-b3-spanid", "x-b3-sampled", "x-request-id")
-	                        .allowedOrigins("*");
-	            }
-	        };
-	    }
-
-
 }
