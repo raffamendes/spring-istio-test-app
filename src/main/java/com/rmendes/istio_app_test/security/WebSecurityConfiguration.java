3b34e33fbd4152e903fef66221985be112a538ba
Raffael Mendes <rmendes@redhat.com>
2024-06-21 14:52:34 -0300

    Trying with spring-boot and without spring-security

--- src/main/java/com/rmendes/istio_app_test/security/WebSecurityConfiguration.java
+++ src/main/java/com/rmendes/istio_app_test/security/WebSecurityConfiguration.java
@@ -1,24 +1,16 @@
 package com.rmendes.istio_app_test.security;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-@EnableWebSecurity
+//@Configuration
+//@EnableWebSecurity
 public class WebSecurityConfiguration {
 	
-	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests(
-				auth -> auth.anyRequest().authenticated()
-				).httpBasic(withDefaults())
-		.headers(headers -> headers.disable());
-		return http.build();
-	}
+//	@Bean
+//	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//		http.authorizeHttpRequests(
+//				auth -> auth.anyRequest().authenticated()
+//				).httpBasic(withDefaults())
+//		.headers(headers -> headers.disable());
+//		return http.build();
+//	}
 
 }
--- src/main/java/com/rmendes/istio_app_test/webconfig/HeadersConfigIstioTest.java
+++ src/main/java/com/rmendes/istio_app_test/webconfig/HeadersConfigIstioTest.java
@@ -1,21 +1,17 @@
 package com.rmendes.istio_app_test.webconfig;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-@EnableWebMvc
-public class HeadersConfigIstioTest implements WebMvcConfigurer{
+//@Configuration
+//@EnableWebMvc
+public class HeadersConfigIstioTest { 
+//implements WebMvcConfigurer{
 	
 	
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/")
-		.allowedOrigins("")
-		.allowedHeaders("Origin", "Access-Control-Allow-Origin", "Content-Type", "Accept", "Authorization", "X-Requested-With", "x-canal", "x-b3-traceid", "x-b3-spanid", "x-b3-sampled", "x-request-id")
-		.allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
-		.maxAge(3600);
-	}
+//	@Override
+//	public void addCorsMappings(CorsRegistry registry) {
+//		registry.addMapping("/")
+//		.allowedOrigins("")
+//		.allowedHeaders("Origin", "Access-Control-Allow-Origin", "Content-Type", "Accept", "Authorization", "X-Requested-With", "x-canal", "x-b3-traceid", "x-b3-spanid", "x-b3-sampled", "x-request-id")
+//		.allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
+//		.maxAge(3600);
+//	}
 }
